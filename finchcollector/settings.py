5cf10c0aa2dd1a1a130acbda3254b97c1bf2e8a0
xkawixgirlx <jennkosh@gmail.com>
2024-05-28 12:35:20 -0700

    Migrate and work on Homepage of Finch Collector, add List of dictionaries for FinchCollector

--- finchcollector/settings.py
+++ finchcollector/settings.py
@@ -9,8 +9,13 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
 from pathlib import Path
+import environ
+environ.Env()
+environ.Env.read_env()
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -75,10 +80,14 @@ WSGI_APPLICATION = 'finchcollector.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+  'default': {
+    'ENGINE': 'django.db.backends.postgresql',
+    'NAME': 'finchcollector',
+    'USER': os.environ['DB_USER'],
+    'PASSWORD': os.environ['DB_PW'],
+    'HOST': os.environ['DB_HOST'],
+    'PORT': '5432',
+  }
 }
 
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
+finches = [
+    {'name': 'Larry', 'type':'canary', 'from': 'Yorkshire', 'color': 'yellow'},
+    {'name': 'Cheryl', 'type':'house-finch', 'from': 'North America', 'color': 'gray'},
+    {'name': 'Toffee', 'type':'towhee', 'from': 'North America', 'color': 'dark-brown'},
+]
+
+
 # Create your views here.
+def home(request):
+    return render(request, 'home.html')
