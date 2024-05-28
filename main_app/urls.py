af5c55bff74d3f8313a61a2fe00334071634d8c2
xkawixgirlx <jennkosh@gmail.com>
2024-05-28 12:59:27 -0700

    Add Finches Page with info on info card

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
-    # path('finches/', views.finches_index, name='finches'),
+    path('finches/', views.finches_index, name='finches'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,7 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+
+def finches_index(request):
+    return render(request, 'finches/index.html', {'finches': finches})
