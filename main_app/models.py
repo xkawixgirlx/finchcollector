01a900ce1154ff4fdff9f0b9f3edc195adc80b50
xkawixgirlx <jennkosh@gmail.com>
2024-05-29 10:10:32 -0700

    Make Model Finch, and make migrations

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Finch(models.Model):
+    name = models.CharField(max_length=100)
+    type = models.CharField(max_length=100)
+    place = models.TextField(max_length=250)
+    color = models.TextField(max_length=250)
+    
+    def __str__(self):
+       return self.name, self.type, self.color
