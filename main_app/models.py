a1cb9fd74f739403bdc65242df4597de3612b745
xkawixgirlx <jennkosh@gmail.com>
2024-05-30 11:47:40 -0700

    Add add feeding functionality, add fed for today box

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', "Breakfast"),
@@ -22,15 +23,21 @@ class Finch(models.Model):
     def get_absolute_url(self):
        return reverse('detail', kwargs={'finch_id': self.id })
    
+    def fed_for_today(self):
+      return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)  
+       
    
 class Feeding(models.Model):
-  date = models.DateField('feeding date')
-  meal = models.CharField(
+    date = models.DateField('feeding date')
+    meal = models.CharField(
         max_length=1,
         choices=MEALS,
         default=MEALS[0][0]
     )
-  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+    finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
   
-  def __str__(self):
+    def __str__(self):
       return f"{self.get_meal_display()} on {self.date}"
+  
+    class Meta:
+      ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Finch
+from .forms import FeedingForm
 
 
 
@@ -20,8 +21,16 @@ def finches_index(request):
 
 def finches_detail(request, finch_id):
     finch = Finch.objects.get(id=finch_id)
-    return render(request, 'finches/detail.html', {'finch': finch})
+    feeding_form = FeedingForm()
+    return render(request, 'finches/detail.html', {'finch': finch, 'feeding_form': feeding_form})
 
+def add_feeding(request, finch_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.finch_id = finch_id
+        new_feeding.save()
+    return redirect('detail', finch_id=finch_id)
 
 class FinchCreate(CreateView):
     model = Finch
